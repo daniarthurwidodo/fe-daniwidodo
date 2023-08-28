2e9a1f462e38ab589c030df6ef9d0988300df54d
daniwidodo <work.daniwidodo87@gmail.com>
2023-08-28 13:41:21 +0700

    update color + add env variable + update readme + remove unused files

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/f8af6459-0857-49ee-869f-2ead1c36cd80/deploy-status)](https://app.netlify.com/sites/dancing-kleicha-722388/deploys)
 
 Things to do :
-- init git
-- add components
-- add asset
-- component test
-- build responsive
-- add mobile menu
-- deploy to netlify
+
 
 live url : https://master--dancing-kleicha-722388.netlify.app/
 run unit test : npm run test
+run dev env : ng serve --configuration=production
+run prod env : ng serve --configuration=production
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
+// import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fe-daniwidodo';
+
+  ngOnInit() {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+  }
+
 }
